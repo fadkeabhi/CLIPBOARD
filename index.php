8a4fa64b74d1504f394a2e9fe005b11de9412b09
Romain <romain@rherault.fr>
2022-10-01 14:52:32 +0200

    feat: add limit changer
    Refs: #8

--- index.php
+++ index.php
@@ -1,31 +1,32 @@
 <?php include 'db.php';
 $msg = " ";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $data = $_POST['data'];
     if (empty($data)) {
-      $msg = "Name is empty";
+        $msg = "Name is empty";
     } else {
         $data = htmlspecialchars($data);
 
-      $sql = "INSERT INTO clips (clip)
-VALUES ('$data')";
+        $sql = "INSERT INTO clips (clip) VALUES ('$data')";
 
-if (mysqli_query($conn, $sql)) {
-    $msg = "Clip added successfully";
-  } else {
-    $msg = "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-
-  
+        if (mysqli_query($conn, $sql)) {
+            $msg = "Clip added successfully";
+        } else {
+            $msg = "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
     }
-  }
+}
 
+// limit of number of clips displayed
+$limit = isset($_GET['show-limit']) ? $_GET['show-limit'] : 5;
 ?>
 
-
-<html>
+<!doctype html>
+<html lang="en">
     <head>
-        <meta charset="utf-8"/>
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>
             MY CLIPBOARD
         </title>
@@ -43,8 +44,8 @@ if (mysqli_query($conn, $sql)) {
         </h1>
 
         <h2>
-    MAKE NEW CLIP
-</h2>
+            MAKE NEW CLIP
+        </h2>
         <form action="" name="submit" method="post">
             <textarea name="data"></textarea>
             <input type="submit" value="Submit">
@@ -54,26 +55,44 @@ if (mysqli_query($conn, $sql)) {
             LATEST CLIPS
         </h2>
 
+        <form action="" name="filter" method="GET">
+            <label for="show-limit">Clips to show :</label>
+
+            <select name="show-limit" id="show-limit">
+                <option value="5" <?= $limit === '5' ? 'selected' : ''?>>5</option>
+                <option value="10" <?= $limit === '10' ? 'selected' : ''?>>10</option>
+                <option value="20" <?= $limit === '20' ? 'selected' : ''?>>20</option>
+                <option value="50" <?= $limit === '50' ? 'selected' : ''?>>50</option>
+                <option value="100" <?= $limit === '100' ? 'selected' : ''?>>100</option>
+                <option value="all" <?= $limit === 'all' ? 'selected' : ''?>>All</option>
+            </select>
+
+            <button type="submit">Show</button>
+        </form>
+
         <h4>
             <?php echo $msg;
             ?>
         </h4>
 
-        
-<?php
-# showing recent clips
-$limit = 5;
-$sql = "SELECT clip FROM clips ORDER BY id DESC LIMIT $limit";
-$result = mysqli_query($conn, $sql);
-$i = 1;
 
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-      echo '<div class="clip"><br>';
-      echo '<p id="clip' . $i . '">' . $row["clip"] . '</p><br></div><br>';
- //     echo '<a href="#" onclick="CopyToClipboard(#clip' . $i . ');return false;">📄</a><br>' ;
-      $i = $i + 1;
+        <?php
+        # showing recent clips
+        if ($limit === 'all') {
+            $sql = "SELECT clip FROM clips ORDER BY id DESC";
+        } else {
+            $sql = "SELECT clip FROM clips ORDER BY id DESC LIMIT $limit";
+        }
+        $result = mysqli_query($conn, $sql);
+        $i = 1;
+
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo '<div class="clip"><br>';
+                echo '<p id="clip' . $i . '">' . $row["clip"] . '</p><br></div><br>';
+                //     echo '<a href="#" onclick="CopyToClipboard(#clip' . $i . ');return false;">📄</a><br>' ;
+                ++$i;
 
 
 
