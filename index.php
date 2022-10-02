eeded634bfd748f03933d324a28e812a6943a177
AranavMahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2022-10-02 16:11:45 +0530

    Update index.php
    Here you can link your social media sites ,it's just a basic footer .

--- index.php
+++ index.php
@@ -113,7 +113,20 @@ $limit = isset($_GET['show-limit']) ? $_GET['show-limit'] : 5;
 
 ?>
 <br>
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-muted">© 2022 Company, Inc</p>
 
+    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+    </a>
 
+    <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+    </ul>
+  </footer>
     </body>
 </html>
