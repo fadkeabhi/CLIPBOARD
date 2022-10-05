9df7c7a07354c17528ea47d343738bd82df174de
B3none <ablackham2000@gmail.com>
2022-10-05 21:43:50 +0100

    continued updates

--- db.php
+++ db.php
@@ -1,11 +1,23 @@
 <?php
 
-require_once __DIR__ . '/config.php';
+$configFile = __DIR__ . '/config.php';
+
+if (!file_exists($configFile)) {
+    die('config.php does not exist');
+}
+
+/**
+ * @var $servername string
+ * @var $username string
+ * @var $password string
+ * @var $dbname string
+ */
+require_once $configFile;
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: {$conn->connect_error}");
+  die("Connection failed: $conn->connect_error");
 }
