094b929a9830359abb0c36a670b6cf005d9358ed
snehafarkya <snhafarkya@gmail.com>
2022-10-06 22:14:21 +0530

    Accepted changes

--- db.php
+++ db.php
@@ -15,7 +15,7 @@ if (!file_exists($configFile)) {
 require_once $configFile;
 
 // Create connection
-$conn = new mysqli("localhost", "root", "sneha", "clipboard");
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
