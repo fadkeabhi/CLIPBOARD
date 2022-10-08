0b59d132a45486e931f51155a853ae29f5092dab
Sahil Sunil Phadtare <97933138+IamsahilPhadtare@users.noreply.github.com>
2022-10-08 12:04:29 +0530

    Update index.php
    Made Changes

--- index.php
+++ index.php
@@ -140,26 +140,21 @@ $limit = $_GET['show-limit'] ?? 5;
         <br>
 
         <script src="./themeswitch.js"></script>
-        <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
-
-        <div style="background-color:black;color:white;padding:30px">Scroll Down</div>
-        <div style="background-color:lightgrey;padding:30px 30px 2500px">
-        <strong>when the user starts to scroll the page</strong>.</div>
-
         <script>
+        function scrollFunction(){
+        if(document.body.scrollTop>20 || document.documentElement.scrollTop>20){
+        scrollbutton.style.display="block";
+        else{
+        scroll.style.display="none";
+        }
+        }
+        function topFunction(){
+        document.body.scrollTop=0;
+        document.documentElement.scrollTop=0;
+        }
+        <button onclick="topFunction()" id="ScrollBtn" title="Go to top">Top</button>
 
-        let mybutton = document.getElementById("myBtn");
-        window.onscroll = function() {scrollFunction()};
-
-        function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-         mybutton.style.display = "block";
-            } else {
-             mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
+        
  
 
 
