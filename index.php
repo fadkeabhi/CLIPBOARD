08fba8d64a15e08604d48433b837140949828ad6
Sahil Sunil Phadtare <97933138+IamsahilPhadtare@users.noreply.github.com>
2022-10-19 15:23:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -141,5 +141,6 @@ $limit = $_GET['show-limit'] ?? 5;
 
         <script src="./themeswitch.js"></script>
         <script src="./clipboard.js"></script>
+        <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
     </body>
 </html>
