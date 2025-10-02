e08ff7c61b35e71dc6e8de91c73420e9f8f61f4f
Terence Eden <edent@users.noreply.github.com>
2025-10-02 21:21:28 +0100

    SRI for bootstrap in footer

--- templates/footer.php
+++ templates/footer.php
@@ -6,7 +6,7 @@
         </div>
     </footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <script>
         // Auto-expand textareas
         document.addEventListener('DOMContentLoaded', function() {
